7b1b3e9017d40189a6ab2826131540076d2507f5
siddharth_boppana@brown.edu <sboppana@vscode1.oscar.ccv.brown.edu>
2024-12-05 01:42:24 -0500

    updated clip text embeddings to work with GPT2

--- unibench/models_zoo/wrappers/clip.py
+++ unibench/models_zoo/wrappers/clip.py
@@ -34,7 +34,7 @@ class ClipModel(AbstractModel):
 
     @torch.no_grad()
     def get_image_embeddings(self, images):
-        image_features = self.model.encode_image(images.to(self.device))
+        image_features = self.model.encode_image({"pixel_values": images.to(self.device)})
         image_features /= image_features.norm(dim=1, keepdim=True)
         return image_features.unsqueeze(1)
 
@@ -45,11 +45,11 @@ class ClipModel(AbstractModel):
             or "truncate" in inspect.getfullargspec(self.tokenizer)[0]
         ):
             caption_tokens = self.tokenizer(
-                captions, context_length=self.context_length, truncate=True
+                captions, max_length = self.context_length, padding = True, padding_side = "left", truncation=True, return_tensors="pt"
             ).to(self.device)
         else:
             caption_tokens = self.tokenizer(
-                captions, context_length=self.context_length
+                captions, max_length = self.context_length, padding = True, padding_side = "left", truncation=True, return_tensors="pt"
             ).to(self.device)
 
         caption_embeddings = self.model.encode_text(caption_tokens)
